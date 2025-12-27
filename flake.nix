91ebe6a37c51481695d260ea06d474d821685c99
kawayww <kawayww@gmail.com>
2025-12-28 00:30:52 +0800

    chore: release v0.6.4

--- flake.nix
+++ flake.nix
@@ -31,7 +31,7 @@
           mkPackage = targetPkgs:
             targetPkgs.rustPlatform.buildRustPackage {
               pname = "tola";
-              version = "0.6.3";
+              version = "0.6.4";
 
               src = ./.;
               cargoLock.lockFile = ./Cargo.lock;
